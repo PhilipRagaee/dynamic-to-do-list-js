e42d46c97be6130e58080764ac3bf4f10943305f
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-07-19 20:10:12 +0300

    Modified Script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
@@ -17,7 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn'); 
+
         removeButton.onclick = function() {
             taskList.removeChild(listItem);
         };
